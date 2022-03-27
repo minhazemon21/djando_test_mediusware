90fdf7e7f2e9c07f1ffa9adb07c82bacd0cede2d
minhazemon21 <minhazkhondokar21@gmail.com>
2022-03-27 15:10:39 +0600

    6th

--- src/product/models.py
+++ src/product/models.py
@@ -1,38 +1,37 @@
 from django.db import models
-from config.g_model import TimeStampMixin
 
 
 # Create your models here.
-class Variant(TimeStampMixin):
-    title = models.CharField(max_length=40, unique=True)
+class Variant(models.Model):
+    title = models.CharField(max_length=40)
     description = models.TextField()
     active = models.BooleanField(default=True)
 
 
-class Product(TimeStampMixin):
+class Product(models.Model):
     title = models.CharField(max_length=255)
-    sku = models.SlugField(max_length=255, unique=True)
+    sku = models.SlugField(max_length=255)
     description = models.TextField()
 
 
-class ProductImage(TimeStampMixin):
+class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     file_path = models.URLField()
 
 
-class ProductVariant(TimeStampMixin):
+class ProductVariant(models.Model):
     variant_title = models.CharField(max_length=255)
-    variant = models.ForeignKey(Variant, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variant = models.ForeignKey(Variant, on_delete=models.CASCADE, related_name="product_variant")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="variants")
 
 
-class ProductVariantPrice(TimeStampMixin):
-    product_variant_one = models.ForeignKey(ProductVariant, on_delete=models.CASCADE, null=True,
+class ProductVariantPrice(models.Model):
+    product_variant_one = models.ForeignKey(ProductVariant, on_delete=models.CASCADE,
                                             related_name='product_variant_one')
-    product_variant_two = models.ForeignKey(ProductVariant, on_delete=models.CASCADE, null=True,
+    product_variant_two = models.ForeignKey(ProductVariant, on_delete=models.CASCADE,
                                             related_name='product_variant_two')
-    product_variant_three = models.ForeignKey(ProductVariant, on_delete=models.CASCADE, null=True,
+    product_variant_three = models.ForeignKey(ProductVariant, on_delete=models.CASCADE,
                                               related_name='product_variant_three')
     price = models.FloatField()
     stock = models.FloatField()
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="variant_price")
